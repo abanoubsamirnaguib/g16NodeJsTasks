c5bc7f5dd1e337cba443c26a87bf2422464ba4c4
abanoubsamirnaguib <abanoubsamir@gmail.com>
2022-03-10 00:59:32 +0200

    last

--- task 1/js/task.js
+++ task 1/js/task.js
@@ -1,7 +1,8 @@
-const addTask = document.querySelector("#addTask")   
+const addUser = document.querySelector("#addtask")   
 const tableBody = document.querySelector("#tableBody")
+
 const tableSingle = document.querySelector("#tableSingle")
-const taskHeads = [ 'name', 'address', 'balance' ]
+const taskHeads = [ 'name',  'balance' , 'address' ]
 //read array data from storage
 const readDataFromStorage= (storageKey)=>{
     let data
@@ -18,15 +19,16 @@ const readDataFromStorage= (storageKey)=>{
 const writeDataToStorage = (data, storageKey)=>{
     localStorage.setItem(storageKey, JSON.stringify(data))
 }
+
 //add Form data to storage
 const formSubmit = function(e){
     e.preventDefault()
     console.log(this.elements);
-    let task = {accNum:Date.now(), createdAt: new Date()} 
-    taskHeads.forEach(head => task[head]= this.elements[head].value )
-    const tasks = readDataFromStorage("tasks")
-    tasks.push(task)
-    writeDataToStorage(tasks, "tasks")
+    let user = {accNum:Date.now(), createdAt: new Date()} 
+    taskHeads.forEach(head => user[head]= this.elements[head].value )
+    const users = readDataFromStorage("users")
+    users.push(user)
+    writeDataToStorage(users, "users")
     this.reset()
     window.location.href="index.html"
 }
@@ -38,12 +40,12 @@ const creatMyOwnElements = (parent, htmlElement, txt, classes)=>{
     if(classes) myEle.className = classes
     return myEle
 }
-//delete single task
-const delTask = (tasks, i) =>{
+//delete single user
+const delTask = (users, i) =>{
     //remove clicked index from array
-    tasks.splice(i, 1)
+    users.splice(i, 1)
     //save data in localStorage
-    writeDataToStorage(tasks,"tasks")
+    writeDataToStorage(users,"users")
     //rebuild table
     showAll()
 }
@@ -54,54 +56,54 @@ const makeEvent = (i, link)=>{
     window.location.href=link
 }
 
-//show single task
-const showSingle = (task, i, tasks ,tableBody1)=>{
-    if(tableBody1){
-            var tr = creatMyOwnElements(tableBody1,"tr",null, null)
+//show single user
+const showSingle = (user, i, users ,tableSingle)=>{
+    if(tableSingle){
+            var tr = creatMyOwnElements(tableSingle,"tr",null, null)
     }
     else{
         var tr = creatMyOwnElements(tableBody,"tr",null, null)
     }
     creatMyOwnElements(tr,"td",i+1, null)
-    const tr2 = creatMyOwnElements(tr,"td",task.accNum , null)
-    taskHeads.forEach(head=>creatMyOwnElements(tr,"td",task[head], null))
+    const tr2 = creatMyOwnElements(tr,"td",user.accNum , null)
+    taskHeads.forEach(head=>creatMyOwnElements(tr,"td",user[head], null))
     const actionTD = creatMyOwnElements(tr,"td",null, null)
     const showBtn =creatMyOwnElements(actionTD, "button", "show", "btn btn-primary me-2")
     const editBtn = creatMyOwnElements(actionTD, "button", "Edit", "btn btn-warning me-2")
     const delBtn = creatMyOwnElements(actionTD, "button", "Delete", "btn btn-danger me-2")
-    delBtn.addEventListener("click", ()=>{ delTask(tasks, i) })
+    delBtn.addEventListener("click", ()=>{ delTask(users, i) })
     // show button logic
     showBtn.addEventListener("click", ()=> makeEvent(i, "single.html"))
     editBtn.addEventListener("click", ()=> makeEvent(i, "edit.html"))
 }
-showAll = () =>{
+const showAll = () =>{
     //reset tbody
-    tableBody.innerHTML=""
+    tableBody.innerHTML="";
     //read data fromstorage
-    const tasks = readDataFromStorage("tasks")
+    const users = readDataFromStorage("users")
     //loop on data
-    tasks.forEach((task, index)=> showSingle(task, index, tasks))
+    users.forEach((user, index)=> showSingle(user, index, users))
 }
-if(addTask) addTask.addEventListener("submit", formSubmit)
+if(addUser) addUser.addEventListener("submit", formSubmit)
 
 if(tableBody) showAll()
 
-const editTask= document.querySelector("#editTask")
-if(editTask){
-    //set tasks as global variable on page
-    const tasks = readDataFromStorage("tasks")
-    //get current task  id from storage
+const edituser= document.querySelector("#editTask")
+if(edituser){
+    //set users as global variable on page
+    const users = readDataFromStorage("users")
+    //get current user  id from storage
     const i = parseInt(localStorage.getItem("showId"))
-    //set form values with my task
-    taskHeads.forEach(head=> editTask.elements[head].value = tasks[i][head])
+    //set form values with my user
+    taskHeads.forEach(head=> edituser.elements[head].value = users[i][head])
     //on submit form
-    editTask.addEventListener("submit", function(e){
+    edituser.addEventListener("submit", function(e){
         //remove form defaults
         e.preventDefault()
         //get data from form and update my array
-        taskHeads.forEach(head => tasks[i][head]= e.target.elements[head].value )
+        taskHeads.forEach(head => users[i][head]= e.target.elements[head].value )
         //update storage
-        writeDataToStorage(tasks, "tasks")
+        writeDataToStorage(users, "users")
         //reset form
         this.reset()
         //redirect to index
@@ -109,12 +111,13 @@ if(editTask){
     })
 }
 if(tableSingle){
-    //read all tasks
-    const tasks = readDataFromStorage("tasks")
-    //get current task  id from storage
+    //read all users
+    const users = readDataFromStorage("users")
+    //get current user  id from storage
     const i = parseInt(localStorage.getItem("showId"))
-    //get task with index find
-    const task = tasks.find( (t,ind) => ind == i )
+    //get user with index find
+    const user = users.find( (t,ind) => ind == i )
     //call show function
-    showSingle(task, i, tasks, tableSingle)
+    showSingle(user, i, users, tableSingle)
 }
+
